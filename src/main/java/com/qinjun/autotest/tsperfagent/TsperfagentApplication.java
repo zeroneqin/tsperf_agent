345f8d790a4d5dd802514483cb32a19db34f7f23
zeroneqin <zeroneqin@gmail.com>
2018-06-24 00:06:52 +0800

    add mvn depend

---
+++ src/main/java/com/qinjun/autotest/tsperfagent/TsperfagentApplication.java
@@ -0,0 +1,12 @@
+package com.qinjun.autotest.tsperfagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsperfagentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsperfagentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/qinjun/autotest/tsperfagent/TsperfagentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qinjun.autotest.tsperfagent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TsperfagentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
